ca3be29003740bb17aff9421c563684a0aaa2415
mritd <mritd1234@gmail.com>
2018-07-27 00:05:36 +0800

    feat(add "go" command): add "go" command to login single server
    add "go" command to login single server
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- cmd/go.go
+++ cmd/go.go
@@ -21,57 +21,20 @@
 package cmd
 
 import (
-	"os"
-
-	"path/filepath"
-
-	"github.com/mitchellh/go-homedir"
 	"github.com/mritd/mmh/pkg/mmh"
-	"github.com/mritd/mmh/pkg/utils"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
-
-var rootCmd = &cobra.Command{
-	Use:   "mmh",
-	Short: "A simple Multi-user ssh tool",
+var goCmd = &cobra.Command{
+	Use:   "go",
+	Short: "Login server",
 	Long: `
-A simple Multi-user ssh tool.`,
+Login server.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		mmh.Run(args)
 	},
 }
 
-func Execute() {
-	utils.CheckAndExit(rootCmd.Execute())
-}
-
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mmh.yaml)")
-
-}
-
-func initConfig() {
-
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		home, err := homedir.Dir()
-		utils.CheckAndExit(err)
-		cfgFile = home + string(filepath.Separator) + ".mmh.yaml"
-		viper.SetConfigFile(cfgFile)
-
-		if _, err := os.Stat(cfgFile); err != nil {
-			os.Create(cfgFile)
-			viper.Set("Servers", mmh.ConfigExample())
-			viper.WriteConfig()
-		}
-
-	}
-	viper.AutomaticEnv()
-	viper.ReadInConfig()
+	rootCmd.AddCommand(goCmd)
 }
