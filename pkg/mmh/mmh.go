af500c3e95914c3d230f4ce3b9a2b35ab21b6b4b
mritd <mritd1234@gmail.com>
2018-07-26 23:24:39 +0800

    feat(login): add SingleLogin
    add SingleLogin
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- pkg/mmh/mmh.go
+++ pkg/mmh/mmh.go
@@ -1,1 +1,25 @@
 package mmh
+
+import (
+	"strings"
+
+	"github.com/mritd/mmh/pkg/utils"
+	"github.com/spf13/viper"
+)
+
+func SingleLogin(name string) {
+	var servers []Server
+	utils.CheckAndExit(viper.UnmarshalKey("servers", &servers))
+	for _, s := range servers {
+		// Ignore case
+		if strings.ToLower(name) == strings.ToLower(s.Name) {
+			s.Connect()
+		}
+	}
+}
+
+func Run(args []string) {
+	if len(args) == 1 {
+		SingleLogin(args[0])
+	}
+}
