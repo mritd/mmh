3b6bb1834136ce375ad3f5781e9887ab1a7f1f1e
mritd <mritd1234@gmail.com>
2018-07-31 00:16:03 +0800

    feat(cp): add remote dir copy
    add remote dir copy
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- pkg/mmh/sftp.go
+++ pkg/mmh/sftp.go
@@ -1,7 +1,6 @@
 package mmh
 
 import (
-	"fmt"
 	"strings"
 
 	"os"
@@ -10,80 +9,118 @@ import (
 
 	"io"
 
+	"path/filepath"
+
+	"errors"
+
 	"github.com/mritd/mmh/pkg/utils"
 	"github.com/pkg/sftp"
-	"golang.org/x/crypto/ssh"
 )
 
-func (s Server) sftpClient1() *sftp.Client {
-	sshConfig := &ssh.ClientConfig{
-		User: s.User,
-		Auth: []ssh.AuthMethod{
-			s.authMethod(),
-		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
-
-	connection, err := ssh.Dial("tcp", fmt.Sprint(s.Address, ":", s.Port), sshConfig)
-	utils.CheckAndExit(err)
-	defer connection.Close()
-
-	c, err := sftp.NewClient(connection)
-	utils.CheckAndExit(err)
-	return c
-}
-
-func (s Server) sftpWrite(localFilePath, remotePath string) {
+func (s Server) fileWrite(localFilePath, remotePath string) {
 
 	localFile, err := os.Open(localFilePath)
 	utils.CheckAndExit(err)
 	defer localFile.Close()
-	info, err := localFile.Stat()
+	_, err = localFile.Stat()
 	utils.CheckAndExit(err)
 
-	if !info.IsDir() {
-		filename := path.Base(localFilePath)
+	filename := path.Base(localFilePath)
 
-		sshClient := s.sshClient()
-		defer sshClient.Close()
+	sshClient := s.sshClient()
+	defer sshClient.Close()
 
-		sftpClient, err := sftp.NewClient(sshClient)
+	sftpClient, err := sftp.NewClient(sshClient)
+	utils.CheckAndExit(err)
+	defer sftpClient.Close()
+
+	if strings.HasPrefix(remotePath, "~") {
+		pwd, err := sftpClient.Getwd()
 		utils.CheckAndExit(err)
-		defer sftpClient.Close()
+		remotePath = strings.Replace(remotePath, "~", pwd, 1)
+	}
+	remoteFileInfo, err := sftpClient.Stat(remotePath)
 
-		if strings.HasPrefix(remotePath, "~") {
-			pwd, err := sftpClient.Getwd()
+	if err != nil {
+		// remotePath is file and not exist
+		if err == os.ErrNotExist {
+			remoteFile, err := sftpClient.Create(remotePath)
 			utils.CheckAndExit(err)
-			remotePath = strings.Replace(remotePath, "~", pwd, 1)
+			defer remoteFile.Close()
+			io.Copy(remoteFile, localFile)
+		} else {
+			// other err
+			utils.Exit(err.Error(), 1)
 		}
-		remoteFileInfo, err := sftpClient.Stat(remotePath)
+	} else {
+		// remotePath is dir
+		if remoteFileInfo.IsDir() {
+			remoteFilePath := path.Join(remotePath, filename)
+			remoteFile, err := sftpClient.Create(remoteFilePath)
+			utils.CheckAndExit(err)
+			defer remoteFile.Close()
+			io.Copy(remoteFile, localFile)
+		} else {
+			utils.Exit("File already exist", 1)
+		}
+	}
+}
 
-		if err != nil {
-			// remotePath is file and not exist
-			if err == os.ErrNotExist {
-				remoteFile, err := sftpClient.Create(remotePath)
-				utils.CheckAndExit(err)
-				defer remoteFile.Close()
-				io.Copy(remoteFile, localFile)
+func (s Server) directoryWrite(localPath, remotePath string) {
+	sshClient := s.sshClient()
+	defer sshClient.Close()
+
+	sftpClient, err := sftp.NewClient(sshClient)
+	utils.CheckAndExit(err)
+	defer sftpClient.Close()
+
+	if strings.HasPrefix(remotePath, "~") {
+		pwd, err := sftpClient.Getwd()
+		utils.CheckAndExit(err)
+		remotePath = strings.Replace(remotePath, "~", pwd, 1)
+	}
+
+	// check remote path is directory
+	remoteFileInfo, err := sftpClient.Stat(remotePath)
+	if err != nil && err != os.ErrNotExist {
+		utils.CheckAndExit(err)
+	} else if err == nil && !remoteFileInfo.IsDir() {
+		utils.Exit("Remote path is not directory", 1)
+	}
+
+	remoteTmpPath := remotePath
+	err = filepath.Walk(localPath, func(path string, info os.FileInfo, err error) error {
+		if info == nil {
+			return err
+		}
+		if info.IsDir() {
+			_, err := sftpClient.Stat(filepath.Join(remoteTmpPath, path))
+			if err != nil {
+				if err == os.ErrNotExist {
+					remoteTmpPath = path
+					return sftpClient.Mkdir(filepath.Join(remotePath, path))
+				}
 			} else {
-				// other err
-				utils.Exit(err.Error(), 1)
+				return errors.New("Remote directory already exist")
 			}
 		} else {
-			// remotePath is dir
-			if remoteFileInfo.IsDir() {
-				remoteFilePath := path.Join(remotePath, filename)
-				remoteFile, err := sftpClient.Create(remoteFilePath)
-				utils.CheckAndExit(err)
-				defer remoteFile.Close()
-				io.Copy(remoteFile, localFile)
-			} else {
-				utils.Exit("File already exist", 1)
-			}
+
 		}
+		return nil
+	})
+}
 
+func (s Server) sftpWrite(localPath, remotePath string) {
+	localFile, err := os.Open(localPath)
+	utils.CheckAndExit(err)
+	defer localFile.Close()
+	localFileInfo, err := localFile.Stat()
+	utils.CheckAndExit(err)
+
+	if localFileInfo.IsDir() {
+		s.directoryWrite(localPath, remotePath)
 	} else {
-
+		s.fileWrite(localPath, remotePath)
 	}
 
 }
@@ -91,6 +128,8 @@ func (s Server) sftpWrite(localFilePath, remotePath string) {
 func Copy(path1, path2 string, singleServer bool) {
 	tmpSp1 := strings.Split(path1, ":")
 	tmpSp2 := strings.Split(path2, ":")
+
+	// download file or dir
 	if len(tmpSp1) == 2 && len(tmpSp2) == 1 {
 		if singleServer {
 			s := findServerByName(tmpSp1[0])
@@ -107,6 +146,7 @@ func Copy(path1, path2 string, singleServer bool) {
 				utils.Exit("Tagged server not found", 1)
 			}
 		}
+		// upload file or dir
 	} else if len(tmpSp1) == 1 && len(tmpSp2) == 2 {
 
 		_, err := os.Stat(path1)
