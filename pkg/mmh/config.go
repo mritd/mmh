11702c32a746a12d0fd802ffd20c92ed34239b6f
mritd <mritd1234@gmail.com>
2018-11-15 22:58:03 +0800

    docs(copyright): format copyright
    format copyright
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- pkg/mmh/config.go
+++ pkg/mmh/config.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 mritd <mritd1234@gmail.com>.
+ * Copyright 2018 mritd <mritd1234@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
